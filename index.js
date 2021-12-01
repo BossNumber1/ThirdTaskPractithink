220ab7ba193f3f70b57f8019dfdcdffb70eb22bb
BossNumber1 <dimaang23@gmail.com>
2021-12-01 19:55:48 +0300

    up

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function dragStart(event) {
     localStorage.setItem("idStarted", event.target.id);
+    localStorage.setItem(
+        "parentElementIdStarted",
+        event.target.parentElement.id
+    );
 }
 
 function allowDrop(event) {
@@ -13,5 +17,12 @@ function drop(event) {
     let objectBeingCreated = document.createElement("div");
     objectBeingCreated.style.marginLeft = "12px";
 
-    event.target.appendChild(objectBeingCreated).appendChild(selectedFigure);
+    event.target.appendChild(objectBeingCreated).appendChild(selectedFigure); // закончили вставку яблока в корзину
+
+    let parentElementIdStarted = localStorage.getItem("parentElementIdStarted");
+    let parElem = document.getElementById(parentElementIdStarted);
+
+    if (parentElementIdStarted === "rowApples") {
+        parElem.children[0].style.opacity = "0.5"; // пометили перенесённый фрукт
+    }
 }
